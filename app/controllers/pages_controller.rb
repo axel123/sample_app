18885f879240595c11c91935a8b7c83b9b0f4926
axel <iamlink@yandex.ru>
2012-05-07 19:23:39 +0400

    Done Static Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contract
+    @title = "Contract"
+  end
+  
+  def about
+    @title = "About"
   end
 end
